af9b83ff898a627e3d228f14b8c2aec3e1e694c3
linus <linushell40@gmail.com>
2024-04-08 10:21:09 +0200

    Lecture 1

--- src/main/java/Vorlesungen/Vorlesung1/App.java
+++ src/main/java/Vorlesungen/Vorlesung1/App.java
@@ -2,6 +2,6 @@ package Vorlesungen.Vorlesung1;
 
 public class App {
     public static void main(String[] args) {
-        System.out.printf("Hello World!\n");
+        System.out.println("Hello World!\n");
     }
 }
